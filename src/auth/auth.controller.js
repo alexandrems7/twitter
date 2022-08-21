4288dde14dd0762d1ed096df377521f443552816
alexandrems7 <alexandremartins770@gmail.com>
2022-08-20 21:06:43 -0300

    login controller and service

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,25 @@
 const authService = require("./auth.service");
-
+const bcrypt = require("bcryptjs");
 const loginController = async (req, res) => {
-  res.send({message: "Login Ok"});
+  const { email, password } = req.body;
+
+  const user = await authService.loginService(email);
+
+  if (!user) {
+    res.status(400).send({ message: "usuario não encontrado." });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+
+  if(!isPasswordValid){
+    return res.status(400).send({message: "Senha inválida!"});
+  }
+
+
+  console.log(isPasswordValid);
+
+  res.send(user);
 };
 
 module.exports = { loginController };
